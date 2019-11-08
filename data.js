13060efc18acc56b82222f1bc5b366f00a544071
Templer <ericTempler@googlemail.com>
2019-11-08 23:59:19 +0100

    Updated Visuals and Full Integration of Bootstrap

--- data.js
+++ data.js
@@ -1,5 +1,12 @@
-var data=[
-    "Zeile 1",
-    "Zeile 2",
-    "Zeile 3"
-]
+//    {text:"text der angezeigt werden soll", func:()=>{alert("LOL");}},
+
+var data={
+    debug:[
+        "Zeile 1",
+        "Zeile 2",
+        'Zeile 4<br>Zeile4.1'
+    ],
+    lol:[
+        "Zeile 1337"
+    ]
+}
--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+var type="debug";
+
+function changeType(t){
+    let but=document.getElementById("dropdownMenuButton");
+    but.innerHTML=t;
+    type=t;
+}
+
 function main(){
-    let index=Math.floor(Math.random()*data.length);
+    let index=Math.floor(Math.random()*data[type].length);
     let div=document.getElementById("effect");
-    div.innerText=data[index];
+    let element=data[type][index];
+    var dat={
+        text:"debugText",
+        func:()=>{}
+    };
+
+    if(typeof(element)=='object'){
+        dat=element;
+    }else{
+        dat.text=element;
+    }
+
+    div.innerHTML=dat.text;
+    dat.func();
 }
 
 main();
