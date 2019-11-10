a2de4f812826f36cbde5428985ad9b44e8d30ba3
Templer <ericTempler@googlemail.com>
2019-11-10 15:51:02 +0100

    Updated Structure and Player

--- data/players.js
+++ data/players.js
@@ -1,6 +1,13 @@
 const players = [
     {
         name: "eric",
+        attacks: [
+            {
+                type: "None",
+                name: "Staff",
+                description: "Has a nob on its end",
+            },
+        ],
         magic: [
             {
                 type: "Daily",
@@ -12,8 +19,28 @@ const players = [
                 name: "test enc",
                 description: "aFASffAFSAf",
             },
+            {
+                type: "Normal",
+                name: "test",
+                description: "Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.",
+            },
         ],
         items: [
+            {
+                type: "Potion",
+                name: "potion",
+                description: "aFASffAFSAf",
+            },
+            {
+                type: "Potion",
+                name: "potion",
+                description: "aFASffAFSAf",
+            },
+            {
+                type: "Potion",
+                name: "potion",
+                description: "aFASffAFSAf",
+            },
             {
                 type: "Potion",
                 name: "potion",
@@ -33,7 +60,7 @@ const players = [
                 type: "Encounter",
                 name: "test enc",
                 description: "aFASffAFSAf",
-            },
+            }
         ],
         items: [
             {
--- players.js
+++ players.js
@@ -1,17 +1,25 @@
-function createTag(tagName, attrs) {
+function createTag(tagName, attrs, parent) {
     const tag = document.createElement(tagName + "");
     for (const attr in attrs) {
         tag.setAttribute(attr + "", attrs[attr + ""] + "");
     }
+
+    if (typeof (parent) !== 'undefined') {
+        parent.appendChild(tag);
+    }
     return tag;
 }
 
-function genCard(obj) {
+function genCard(obj, classes) {
     const name=obj.name;
     const description=obj.description;
     const type=obj.type;
+
+    if (typeof (classes) === 'undefined') {
+        classes = "";
+    }
     
-    const card = createTag("div", {class: "card "+type, style:"width: 30%"});
+    const card = createTag("div", {class: "card "+type+" "+classes, style:"width: 30%"});
     const head = createTag("h5", {class: "card-header"});
     head.innerText = name;
     card.appendChild(head);
@@ -23,9 +31,37 @@ function genCard(obj) {
 }
 
 function genPlayer(obj) {
+    function createLI(controlID, player,  nav, active) {
+        const tab = createTag("li", {class: "nav-controllID"});
+        const a = createTag("a", {
+            class: active ? "nav-link active" : "nav-link",
+            id: player + "-" + controlID + "-tab",
+            href: "#tab" + controlID,
+            "data-toggle": "tab",
+            role: "tab",
+            "aria-controls": "nav-" + player + "-" + controlID,
+            "aria-selected": active ? "true" : "false"
+        });
+        a.innerText = controlID;
+        tab.appendChild(a);
+        nav.appendChild(tab);
+    }
+
+    function createPane(controlID, player, list, tabPaneContend, active) {
+        const tabPaneItem = createTag("div", {
+            class: "tab-pane fade show" + (active ? " active" : ""),
+            id: "tab" + controlID,
+            role: "tabpanel",
+            "aria-labelledby": player + "-" + controlID + "-tab"
+        }, tabPaneContend);
+        for (const obj in list) {
+            tabPaneItem.appendChild(genCard(list[obj]));
+        }
+    }
+
     const name = obj.name;
     const card = createTag("div", {class: "card"});
-    const header = createTag("div", {class: "card-header", id: "heading-" + name});
+    const header = createTag("div", {class: "card-header", id: "heading-" + name}, card);
     const but = createTag("button", {
         class: "btn btn-link",
         type: "button",
@@ -33,7 +69,7 @@ function genPlayer(obj) {
         "data-target": "#collapse-" + name,
         "aria-expanded": "true",
         "aria-controls": "collapseOne"
-    });
+    }, header);
     but.innerText = name;
 
     const collapse = createTag("div", {
@@ -41,18 +77,19 @@ function genPlayer(obj) {
         class: "collapse",
         "aria-labelledby": "headingOne",
         "data-parent": "#accordion"
-    });
-    const collapseBody = createTag("div", {"class": "card-body", style:"display: block ruby;"});
-    for (const item in obj.items) {
-        const m = obj.items[item];
-        collapseBody.appendChild(genCard(m));
-    }
-    
-    collapse.appendChild(collapseBody);
+    }, card);
+    const collapseBody = createTag("div", {"class": "card-body"}, collapse);
 
-    header.appendChild(but);
-    card.appendChild(header);
-    card.appendChild(collapse);
+    const nav = createTag("ul", {class: "nav nav-tabs", id: "Tab" + name, role: "tablist"}, collapseBody);
+    createLI("item", name, nav, true);
+    createLI("magic", name, nav);
+    createLI("Weapons", name, nav);
+
+    const tabPaneContend = createTag("div", {class: "tab-content", id: "Tab" + name + "-Contend"}, collapseBody);
+    createPane("item", name, obj.items, tabPaneContend, true);
+    createPane("magic", name, obj.magic, tabPaneContend);
+    createPane("Weapons", name, obj.attacks, tabPaneContend);
+    
     return card;
 }
 
